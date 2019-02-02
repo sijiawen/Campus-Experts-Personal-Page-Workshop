7ebab3b88a5f0de4fc901c7666e062779eeb5c5b
Si Jia <Sijia@Sis-MacBook-Air.local>
2019-02-02 14:29:24 -0500

    deploy

--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,7 @@
 function showContactForm(){
-    var view = document.getElementById("contact-form").getAttribute('display'); //mistake? getComputedStyle
-    //var view = getComputedStyle(document.getElementById("contact-form")).display;
+    //var view = document.getElementById("contact-form").getAttribute('display'); //mistake? getComputedStyle
+    var view = getComputedStyle(document.getElementById("contact-form")).display;
+    console.log("broken");
     document.getElementById("contact-form").style.display = (view === "none" ? "block": "none");
 
 }
